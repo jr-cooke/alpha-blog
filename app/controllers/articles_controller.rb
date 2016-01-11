cf466ea4c3301e5521e2ec5508ae92e9c81d258b
Justin Cooke <jrcooke2@gmail.com>
2016-01-11 18:31:45 +0000

    Styled flash messages and errors partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article Created"
+      flash[:success] = "Article Created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article Updated"
+      flash[:success] = "Article Updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def destroy 
     @article = set_article
     @article.destroy
-    flash[:notice] = "Article Deleted"
+    flash[:danger] = "Article Deleted"
     redirect_to articles_path
   end
   
